99152b11bf7b679244de422d233d4c3870408e64
AnnaWielgusiak <58111126+AnnaWielgusiak@users.noreply.github.com>
2022-03-11 17:55:00 +0100

    carousel

---
+++ js/carousel.js
@@ -0,0 +1,36 @@
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+    let $e = $(e.relatedTarget);
+    let idx = $e.index();
+    let itemsPerSlide = 4;
+    let totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        let it = itemsPerSlide - (totalItems - idx);
+        for (let i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+  $('#carouselExample').carousel({ 
+                interval: 2000
+        });
+
+  $(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function(event){
+      event.preventDefault();
+      var content = $('.modal-body');
+      content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);        
+        content.html($(this).html());
+        $(".modal-profile").modal({show:true});
+    });
+  });
