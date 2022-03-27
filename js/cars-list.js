d0a103df68327f4b939fc28c7f806621ab8eb24b
AnnaWielgusiak <58111126+AnnaWielgusiak@users.noreply.github.com>
2022-03-27 19:40:47 +0200

    fill car list

--- js/cars-list.js
+++ js/cars-list.js
@@ -1,32 +1,32 @@
 console.log(CARS);
 
 document.addEventListener("DOMContentLoaded", function ($event) {
+  fillCarlist();
+});
+
+const fillCarlist = () => {
+  let carImage = document.getElementsByClassName("car-image-div");
+  let imgArray = new Array();
+  let carItem = document.getElementsByClassName("car-item");
+  let carYear = document.getElementsByClassName("year");
+  let carOdometer = document.getElementsByClassName("odometer");
+  let carPower = document.getElementsByClassName("power");
+  let carPrice = document.getElementsByClassName("price2");
 
 
+  for (let i=0; i < CARS.length; i++) {
+    imgArray[i] = new Image();
+    imgArray[i].src = CARS[i].picture;
+    imgArray[i].alt = `${CARS[i].brand} ${CARS[i].model}`; 
+    imgArray[i].classList.add("car-image");
+    console.log(imgArray[i])
+    carImage[i].appendChild(imgArray[i]);
+    carItem[i].innerText = `${CARS[i].brand} / ${CARS[i].model}`;
+    carYear[i].innerText = `${CARS[i].year} rok`
+    carOdometer[i].innerText = `${CARS[i].odometer} km`;
+    carPower[i].innerText = `${CARS[i].power} KM`;
+    carPrice[i].innerText = `${CARS[i].price} zł`; 
+  
+  }
 
-    let carData = document.getElementsByClassName("car-info");
-    carData[0].innerText = `${CARS[0].brand} ${CARS[0].model}`;
-    let carFeatures = document.getElementsByClassName("car-features");
-    carFeatures[0].innerText = `${CARS[0].year} ${CARS[0].odometer} ${CARS[0].power}`;
-    let carPrice = document.getElementsByClassName("car-price");
-    carPrice[0].innerText = `${CARS[0].price}`;
-    let carImage = document.getElementsByClassName("car-image");
-    let image = new Image();
-    image.src = CARS[0].picture;
-    image.alt = `${CARS[0].brand} ${CARS[0].model}`;
-    image.classList.add("car-image");
-    carImage[0].appendChild(image);
-
-    let carData = document.getElementsByClassName("car-info");
-    carData[0].innerText = `${CARS[0].brand} ${CARS[0].model}`;
-    let carFeatures = document.getElementsByClassName("car-features");
-    carFeatures[0].innerText = `${CARS[0].year} ${CARS[0].odometer} ${CARS[0].power}`;
-    let carPrice = document.getElementsByClassName("car-price");
-    carPrice[0].innerText = `${CARS[0].price}`;
-    let carImage = document.getElementsByClassName("car-image");
-    let image = new Image();
-    image.src = CARS[0].picture;
-    image.alt = `${CARS[0].brand} ${CARS[0].model}`;
-    image.classList.add("car-image");
-    carImage[0].appendChild(image);
-   });
+};
